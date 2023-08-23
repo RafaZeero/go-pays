5c5dda0ef80a3406a83defd71433bb19f5359ec0
RafaZeero <rafaa99@gmail.com>
2023-08-23 13:28:24 -0300

    start proj

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+// import "net/http"
+
+func main() {
+	// http.HandleFunc("/accounts", getAccounts)
+	// http.HandleFunc("/accounts", createAccount)
+	// http.HandleFunc("/accounts/deposit", deposit)
+	// http.HandleFunc("/accounts/withdrawl", withdrawl)
+}
