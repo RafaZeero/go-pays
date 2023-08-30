85c4a4247b13c3f2a2d8826146e41d206b475323
RafaZeero <rafaa99@gmail.com>
2023-08-30 16:57:54 -0300

    add helper func

--- handler/handler.go
+++ handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/RafaZeero/go-pays/config"
 )
@@ -14,4 +15,13 @@ var (
 func InitHandler() {
 	logger = config.GetLogger("handler")
 	db = config.GetDB()
+}
+
+func ParseDBDate(dbDate []uint8) (time.Time, error) {
+	dateString := string(dbDate)
+	parsedTime, err := time.Parse("2006-01-02 15:04:05", dateString)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return parsedTime, nil
 }
