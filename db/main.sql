e2fda91b8f58bb97b0a7b0be36d0015f1904b8dc
RafaZeero <rafaa99@gmail.com>
2023-08-30 16:57:46 -0300

    fix .sql file

--- db/main.sql
+++ db/main.sql
@@ -9,6 +9,6 @@ CREATE TABLE accounts(
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
   updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-INSERT INTO accounts(name, balance, createdAt) VALUES('Rafa', 15000.99, NOW());
-INSERT INTO accounts(name, balance, createdAt) VALUES('Rayssa', 500, NOW());
-INSERT INTO accounts(name, balance, createdAt) VALUES('Dandan', 0, NOW());
+INSERT INTO accounts(name, balance, createdAt, updatedAt) VALUES('Rafa', 15000.99, NOW(), NOW());
+INSERT INTO accounts(name, balance, createdAt, updatedAt) VALUES('Rayssa', 500, NOW(), NOW());
+INSERT INTO accounts(name, balance, createdAt, updatedAt) VALUES('Dandan', 0, NOW(), NOW());
