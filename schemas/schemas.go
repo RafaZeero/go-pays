d64e351c644c023cb491277532ec2670d97efb72
RafaZeero <rafaa99@gmail.com>
2023-08-30 16:35:48 -0300

    update schemas

--- schemas/schemas.go
+++ schemas/schemas.go
@@ -1,1 +1,17 @@
 package schemas
+
+import "time"
+
+type Account struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Balance   float64   `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type User struct {
+	ID      int64  `json:"id"`
+	Name    string `json:"name"`
+	Balance string `json:"balance"`
+}
