5f31c6aece807c630cf7ab807bbb5f3ee8dbc52d
RafaZeero <rafaa99@gmail.com>
2023-08-30 16:36:34 -0300

    fix get all accounts

--- handler/get-all-accounts.go
+++ handler/get-all-accounts.go
@@ -3,19 +3,20 @@ package handler
 import (
 	"net/http"
 
+	"github.com/RafaZeero/go-pays/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func GetAccounts(ctx *gin.Context) {
-	rows, _ := db.Query("select id, name, balance from accounts")
+	rows, _ := db.Query("select id, name, balance, createdAt, updatedAt from accounts")
 	defer rows.Close()
 
 	// All users
-	var us []User
+	var us []schemas.Account
 
 	for rows.Next() {
-		var u User
-		rows.Scan(&u.ID, &u.Name, &u.Balance)
+		var u schemas.Account
+		rows.Scan(&u.ID, &u.Name, &u.Balance, &u.CreatedAt, &u.UpdatedAt)
 		us = append(us, u)
 	}
 	// Response: Get accounts variable
