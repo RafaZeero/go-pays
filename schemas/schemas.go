693918a156e0e1cece3840503a53131bf607fddf
RafaZeero <rafaa99@gmail.com>
2023-08-31 11:04:12 -0300

    refactor schemas

--- schemas/schemas.go
+++ schemas/schemas.go
@@ -3,15 +3,13 @@ package schemas
 import "time"
 
 type Account struct {
-	ID        int64     `json:"id"`
-	Name      string    `json:"name"`
-	Balance   float64   `json:"balance"`
+	User
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 type User struct {
-	ID      int64  `json:"id"`
-	Name    string `json:"name"`
-	Balance string `json:"balance"`
+	ID      int64   `json:"id"`
+	Name    string  `json:"name"`
+	Balance float64 `json:"balance"`
 }
