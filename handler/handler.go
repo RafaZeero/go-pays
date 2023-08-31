2e94e4cc88b9e96913803b1249798bbbe08dad59
RafaZeero <rafaa99@gmail.com>
2023-08-31 12:03:41 -0300

    add helpers for handler

--- handler/handler.go
+++ handler/handler.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"database/sql"
-	"time"
 
 	"github.com/RafaZeero/go-pays/config"
 )
@@ -15,13 +14,4 @@ var (
 func InitHandler() {
 	logger = config.GetLogger("handler")
 	db = config.GetDB()
-}
-
-func ParseDBDate(dbDate []uint8) (time.Time, error) {
-	dateString := string(dbDate)
-	parsedTime, err := time.Parse("2006-01-02 15:04:05", dateString)
-	if err != nil {
-		return time.Time{}, err
-	}
-	return parsedTime, nil
 }
---
+++ handler/helpers.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"math/big"
+	"time"
+)
+
+// Ref: https://stackoverflow.com/questions/54362751/how-can-i-truncate-float64-number-to-a-particular-precision#:~:text=is%200.3...).-,Using%20big.Float,-To%20properly%20truncate
+func Truncate(f float64, unit float64) float64 {
+	bf := big.NewFloat(0).SetPrec(1000).SetFloat64(f)
+	bu := big.NewFloat(0).SetPrec(1000).SetFloat64(unit)
+
+	bf.Quo(bf, bu)
+
+	// Truncate:
+	i := big.NewInt(0)
+	bf.Int(i)
+	bf.SetInt(i)
+
+	f, _ = bf.Mul(bf, bu).Float64()
+	return f
+}
+
+func ParseDBDate(dbDate []uint8) (time.Time, error) {
+	dateString := string(dbDate)
+	parsedTime, err := time.Parse("2006-01-02 15:04:05", dateString)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return parsedTime, nil
+}
