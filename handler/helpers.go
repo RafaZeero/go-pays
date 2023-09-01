6451cefd9eb7caf1765211804f62d005d5d7a05e
RafaZeero <rafaa99@gmail.com>
2023-09-01 09:01:40 -0300

    add another reference for decimals

--- handler/helpers.go
+++ handler/helpers.go
@@ -6,6 +6,7 @@ import (
 )
 
 // Ref: https://stackoverflow.com/questions/54362751/how-can-i-truncate-float64-number-to-a-particular-precision#:~:text=is%200.3...).-,Using%20big.Float,-To%20properly%20truncate
+// Take a look at https://github.com/shopspring/decimal
 func Truncate(f float64, unit float64) float64 {
 	bf := big.NewFloat(0).SetPrec(1000).SetFloat64(f)
 	bu := big.NewFloat(0).SetPrec(1000).SetFloat64(unit)
