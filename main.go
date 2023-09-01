bdf5972c57d49a71131c4136cff83d833d66c9a1
RafaZeero <rafaa99@gmail.com>
2023-09-01 09:56:53 -0300

    remove unused code

--- main.go
+++ main.go
@@ -23,35 +23,3 @@ func main() {
 	// Initialize Router
 	router.InitRouter()
 }
-
-// #region insert
-// func insert(db *sql.DB) {
-// 	// Init transaction
-// 	tx, _ := db.Begin()
-// 	// Statement
-// 	stmt, _ := tx.Prepare("insert into accounts (name, balance) values (?,?)")
-// 	stmt.Exec("RafaZeero3", 1456)
-// 	stmt.Exec("Luffy3", 2789)
-// 	_, err := stmt.Exec("goku2", 34770)
-// 	// Rollback if an error occur
-// 	if err != nil {
-// 		tx.Rollback()
-// 		log.Fatal(err)
-// 	}
-// 	// Commit transaction
-// 	tx.Commit()
-// }
-// #endregion insert
-
-// #region select
-// func selectFromDB(db *sql.DB) {
-// 	rows, _ := db.Query("select id, name, balance from accounts where id > ?", 2004)
-// 	defer rows.Close()
-// 	for rows.Next() {
-// 		var u User
-// 		rows.Scan(&u.ID, &u.Name, &u.Balance)
-// 		fmt.Println(u)
-// 	}
-// }
-
-// #endregion select
